0d2d39dff326b4dcf4ecf5873b12ace0720ebf8e
rafly030 <116357029+rafly030@users.noreply.github.com>
2023-03-10 16:02:12 +0700

    Add files via upload

---
+++ sesi 4 java/number1.java
@@ -0,0 +1,8 @@
+public class number1 {
+    public static void main(String[] args){
+        for (int i=1; i<=11; i++){
+            System.out.print(i+" ");
+            i++;
+    }
+    }
+}
---
+++ sesi 4 java/number2.java
@@ -0,0 +1,7 @@
+public class number2 {
+    public static void main(String[] args){
+        for (int a=20; a>=0; a-=5){
+            System.out.print(a+" ");
+        }
+    }
+}
